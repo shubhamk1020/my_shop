8dd49163f2036b9f7527f05065da427724d82ed1
shubham1020k <gyaninfo128@gmail.com>
2022-09-02 10:09:45 +0530

    Intial Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './Screen/product_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(primarySwatch: Colors.blue,
+      ),
+      home: ProductOverviewScreen()
+    );
+  }
+}
